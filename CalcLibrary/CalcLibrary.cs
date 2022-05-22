b9e0f19998b3784da34ff55921dd4e7d49e7d1db
AVAtar Mod <respawner289@gmail.com>
2022-05-22 19:13:17 +0300

    Add DoOperation implementation

--- CalcLibrary/CalcLibrary.cs
+++ CalcLibrary/CalcLibrary.cs
@@ -1,26 +1,47 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 using Extensions;
 
 namespace CalcLibrary
 {
 
+#if DEBUG
+    public
+#endif
+    delegate double OperationDelegate(double x, double y);
     static public class Calc
     {
+
+#if DEBUG
+        public
+#endif
+        static readonly Dictionary<string, OperationDelegate> DoubleOperation =
+        new Dictionary<string, OperationDelegate>
+        {{ "+", (x,y) => x + y},
+        { "-", (x, y) => x - y },
+        { "*", (x, y) => x * y},
+        { "/", (x, y) => x / y }};
+
         private const string _operationPattern = @"(?!-\d)[^\d,\.]";
         private static readonly Func<Match, int, string> _convertMatchToString = (match, i) => match.Value;
-        private static readonly Regex regex = new Regex(_operationPattern);
+        private static readonly Regex _operationRegex = new Regex(_operationPattern);
 
 #if DEBUG
         public
 #endif
         static string[] GetOperands(string input)
         {
-            MatchCollection collection = regex.Matches(input);
+            MatchCollection collection = _operationRegex.Matches(input);
 
-            return input.Split(collection.Select(_convertMatchToString).ToArray<string>(), StringSplitOptions.None);
+            string[] result = input.Split(collection.Select(_convertMatchToString).ToArray<string>(), StringSplitOptions.None);
+
+            DebugConsole.WriteLine($"Operands: `{result[0]}`,`{result[1]}`");
+
+            return result;
         }
 
 #if DEBUG
@@ -29,9 +50,9 @@ namespace CalcLibrary
         static string GetOperation(string input)
         {
             input = input.Replace(" ", string.Empty);
-            MatchCollection collection = regex.Matches(input);
+            MatchCollection collection = _operationRegex.Matches(input);
 
-            DebugConsole.WriteLine($"Result: `{string.Join(" ", collection.Select(_convertMatchToString))}`\nLength: {collection.Count}");
+            DebugConsole.WriteLine($"Operation: `{string.Join(" ", collection.Select(_convertMatchToString))}`");
 
             if (collection.Count > 1)
                 throw new ArgumentException("Bad expression format, there is must be one operation per expression", "input");
@@ -40,7 +61,51 @@ namespace CalcLibrary
         }
         public static string DoOperation(string s)
         {
-            return "";
+            string[] array = GetOperands(s);
+            CultureInfo real = GetCultureInfo(array);
+            CultureInfo.CurrentCulture = real;
+            double[] operands = Array.ConvertAll<string, double>(array, i => double.Parse(i));
+
+            string op = GetOperation(s);
+
+            double result = DoubleOperation[op].Invoke(operands[0], operands[1]);
+
+            DebugConsole.WriteLine("> DoOperation start\n" +
+                $"\tDecimal separator: `{real.NumberFormat.NumberDecimalSeparator}`\n" +
+                $"\tOperands (string): `{array[0]}`,`{array[1]}`\n" +
+                $"\tOperands: `{operands[0]}`,`{operands[1]}`\n" +
+                $"\tOperator: `{op}`\n" +
+                $"\tResult: {result}\n" +
+                "> DoOperation end"
+                );
+
+            return $"{Math.Round(result, 3)}";
+        }
+
+        private static CultureInfo GetCultureInfo(string[] array)
+        {
+            Regex separatorRegex = new Regex(@"[^\d]");
+            CultureInfo info = new CultureInfo(CultureInfo.CurrentCulture.Name);
+            NumberFormatInfo tmp = new NumberFormatInfo();
+
+            bool first = true;
+            foreach (string i in array)
+            {
+                MatchCollection collection = separatorRegex.Matches(i);
+                if (collection.Count > 1)
+                    throw new FormatException("Invalid number format");
+                else if (collection.Count == 1 && collection[0].Value.Length == 1)
+                {
+                    tmp.NumberDecimalSeparator = collection[0].Value;
+                    if (first)
+                        info.NumberFormat.NumberDecimalSeparator = tmp.NumberDecimalSeparator;
+                    else if (tmp.NumberDecimalSeparator != info.NumberFormat.NumberDecimalSeparator)
+                        throw new FormatException("All numbers in expression must have same decimal separator");
+                    first = false;
+                }
+            }
+
+            return info;
         }
     }
 }
