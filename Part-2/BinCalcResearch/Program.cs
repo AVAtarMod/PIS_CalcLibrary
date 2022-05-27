cc75c352c599df5e9eb86c238f3a45dbc2c34a13
AVAtarMod <Respawner289@gmail.com>
2022-05-27 09:47:51 +0300

    Add library research project

---
+++ Part-2/BinCalcResearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BinCalcResearch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("BinCalc.dll");
+
+            Type[] assemblyTypes = assembly.GetTypes();
+            Console.WriteLine("Типы:");
+            assemblyTypes.ToList().ForEach((i) => { Console.WriteLine("\t" + i.Name + " " + i.FullName); });
+
+            FieldInfo[] ts = assemblyTypes[0].GetFields();
+            Console.WriteLine("Публичные поля (" + ts.Length + " штук) " + assemblyTypes[0].Name + ":");
+            ts.ToList().ForEach((i) => { Console.WriteLine("\t" + i.FieldType.Name + " " + i.Name); });
+
+            MethodInfo[] ms = assemblyTypes[0].GetMethods();
+            Console.WriteLine("Методы " + assemblyTypes[0].Name + ":");
+            ms.ToList().ForEach((i) => { Console.WriteLine("\t" + i.DeclaringType + "::" + i.Name); });
+
+            MethodInfo method = assemblyTypes[0].GetMethods()[0];
+            Console.WriteLine("Сигнатура " + assemblyTypes[0].Name + "::" + method.Name + ":");
+            string parameters = string.Join("", method.GetParameters().Select(i => i.Name + ",  "));
+            parameters = parameters.Remove(parameters.Length - 3);
+            Console.WriteLine("\t" + method.ReturnType.Name + " " + method.Name + " (" + parameters + ")");
+
+            Console.ReadKey();
+        }
+    }
+}
